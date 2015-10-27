f85e0079c2767f5f8c15af65bf9e422d088bce3c
YPench <ypench@gmail.com>
2015-10-27 15:54:17 +0800

    Approved license

--- Consensus/Consensus/Consensus_Dlg.h
+++ Consensus/Consensus/Consensus_Dlg.h
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2015 by Yanping Chen <ypench@gmail.com>
+ * Begin       : 01-Oct-2015
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser GPL (LGPL) as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program.
+ */
+
 // Consensus_Dlg.h : header file
 //
 
